2a48b38e8d74f1023f9242b05d138a4c9f882652
luishbarros <luishrbr@outlook.com>
2024-03-18 16:02:31 -0300

    Hash_functions

--- user/src/services/registerAdm.ts
+++ user/src/services/registerAdm.ts
@@ -1,22 +1,24 @@
 import { AdminRepository } from "../repositories/admRepository";
 import { CacheRepository } from "../repositories/cacheRepository";
 import { UsersRepository } from "../repositories/usersRepository";
-
-
+import Hash from "../utils/Hash";
 
 interface RegisterAdministrator {
     voucher: string;
     cel: string;
     username: string;
     password: string;
-
 }
 
 export default class registerAdm {
-    constructor(private usersRepository: UsersRepository, private adminRepository: AdminRepository, private cacheRepository: CacheRepository) { }
+    constructor(
+        private usersRepository: UsersRepository,
+        private adminRepository: AdminRepository,
+        private cacheRepository: CacheRepository,
+        private hashFunctions: Hash
+    ) {}
 
     async execute(data: RegisterAdministrator) {
-
         const user = await this.usersRepository.findByCel(data.cel);
         if (!user) {
             throw new Error("User didn't exists");
@@ -31,11 +33,9 @@ export default class registerAdm {
         }
         const newAdmin = await this.adminRepository.create({
             username: data.username,
-            password: Hash(data.password), //To do a Hash function
-            userCel: data.cel
+            password: this.hashFunctions.encrypt(data.password),
+            userCel: data.cel,
         });
         return newAdmin;
-
     }
-
 }
---
+++ user/src/utils/Hash.ts
@@ -0,0 +1,16 @@
+import { createCipheriv, createDecipheriv, randomBytes } from "node:crypto";
+
+const pass = randomBytes(32);
+const iv = randomBytes(16);
+// Must be gen when the application starts.
+
+export default class Hash {
+    encrypt(message: string): string {
+        const encrypt = createCipheriv("aes-256-gcm", pass, iv);
+        return encrypt.update(message, "utf-8", "base64");
+    }
+    desencrypt(message: string): string {
+        const decipher = createDecipheriv("aes-256-gcm", pass, iv);
+        return decipher.update(message, "utf-8", "base64");
+    }
+}
