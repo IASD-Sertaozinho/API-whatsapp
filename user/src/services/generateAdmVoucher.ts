218985362845529700a9ca52ac54efcade9aeae4
LuisHBarros <luishrbr@outlook.com>
2024-03-19 21:49:20 +0000

    VoucherServiceFix and GenVoucherTestFix

--- user/src/services/generateAdmVoucher.ts
+++ user/src/services/generateAdmVoucher.ts
@@ -1,26 +1,39 @@
 import { UserAlreadyIsAnAdministrator } from "../errors/UserAlreadyIsAnAdministrator";
 import { UserDidntExists } from "../errors/UserDidntExists";
+import UserIsntAnAdminError from "../errors/UserIsntAnAdmin";
 import { AdminRepository } from "../repositories/admRepository";
 import { CacheRepository } from "../repositories/cacheRepository";
 import { UsersRepository } from "../repositories/usersRepository";
 import { generate } from "voucher-code-generator";
+
+interface generateAdmVoucher {
+    newAdminNumber: string,
+    adminNumber: string,
+}
 
 export class GenerateAdmVoucher {
     constructor(
         private usersRepository: UsersRepository,
         private adminRepository: AdminRepository,
         private cacheRepository: CacheRepository
-    ) {}
+    ) { }
+
 
     //Needs a Middleware to check if the request user is an Admin
-    async execute(newAdminCel: string) {
-        const user = await this.usersRepository.findByCel(newAdminCel);
+    async execute(data: generateAdmVoucher) {
+
+        const isAdm = await this.adminRepository.findByUserNumber(data.adminNumber);
+        if (!isAdm) {
+            throw new UserIsntAnAdminError("You are not an Administrator!");
+        }
+
+        const user = await this.usersRepository.findByCel(data.newAdminNumber);
         if (!user) {
             throw new UserDidntExists("User not found");
         }
 
         const isAdmin = await this.adminRepository.findByUserNumber(
-            newAdminCel
+            data.newAdminNumber
         );
         if (isAdmin) {
             throw new UserAlreadyIsAnAdministrator(
@@ -34,7 +47,7 @@ export class GenerateAdmVoucher {
 
         const setTimeout = 600;
 
-        await this.cacheRepository.set(newAdminCel, voucher[0], setTimeout);
+        await this.cacheRepository.set(data.newAdminNumber, voucher[0], setTimeout);
         return voucher[0];
     }
 }
--- user/src/services/tests/generateAdmVoucher.spec.ts
+++ user/src/services/tests/generateAdmVoucher.spec.ts
@@ -18,6 +18,7 @@ describe("GenerateAdmVoucher", () => {
     let usersRepository: UsersRepository;
     let adminRepository: AdminRepository;
     let cacheRepository: CacheRepository;
+    let adm_phone_number: string;
 
     beforeEach(async () => {
         usersRepository = new InMemoryUsersRepository();
@@ -29,14 +30,14 @@ describe("GenerateAdmVoucher", () => {
             cacheRepository
         );
         registerUser = new RegisterUser(usersRepository);
-        const phone_number = "123456789";
+        adm_phone_number = "123456789";
         await registerUser.execute({
-            cel: phone_number,
+            cel: adm_phone_number,
             name: "John Doe",
             message: Message.COMUM,
         });
         await adminRepository.create({
-            cel: phone_number,
+            cel: adm_phone_number,
             password: "123456",
         });
     });
@@ -48,25 +49,25 @@ describe("GenerateAdmVoucher", () => {
             message: Message.COMUM,
         });
         console.log("here");
-        const response = await generateAdmVoucher.execute("1234567890");
+        const response = await generateAdmVoucher.execute({ adminNumber: adm_phone_number, newAdminNumber: "1234567890" });
         expect(typeof response).toBe("string");
     });
 
     it("should not generate a new admin voucher for an user that doesn't exists", async () => {
         expect(async () => {
-            await generateAdmVoucher.execute("1234567890");
+            await generateAdmVoucher.execute({ adminNumber: adm_phone_number, newAdminNumber: "1234123123" });
         }).rejects.toBeInstanceOf(UserDidntExists);
     });
 
     it("should not generate a new admin voucher for an user that is already an admin", async () => {
         expect(async () => {
-            await generateAdmVoucher.execute("123456789");
+            await generateAdmVoucher.execute({ adminNumber: adm_phone_number, newAdminNumber: adm_phone_number });
         }).rejects.toBeInstanceOf(UserAlreadyIsAnAdministrator);
     });
 
     it("should not generate a new admin voucher as a User that is not an admin", async () => {
         expect(async () => {
-            await generateAdmVoucher.execute("123456789");
-        }).rejects.toBeInstanceOf(UserIsntAnAdminError);
+            await generateAdmVoucher.execute({ adminNumber: "12345678912", newAdminNumber: "1234567893" });
+        }).rejects.toBeInstanceOf(UserIsntAnAdminError)
     });
 });
