21a1a3b44f0072ae6c8a8b6de638015e66346cb7
LuisHBarros <luishrbr@outlook.com>
2024-03-19 22:39:08 +0000

    Work around (need to refactor)

--- user/src/app.ts
+++ user/src/app.ts
@@ -1,3 +1,9 @@
 import fastify from "fastify";
+import Hash from "./utils/Hash";
+import { randomBytes } from "node:crypto";
 
 export const app = fastify();
+
+//Must have a better way to implement this...
+export const hash = new Hash(randomBytes(32),
+    randomBytes(16));
--- user/src/utils/Hash.ts
+++ user/src/utils/Hash.ts
@@ -1,16 +1,23 @@
 import { createCipheriv, createDecipheriv, randomBytes } from "node:crypto";
 
-const pass = randomBytes(32);
-const iv = randomBytes(16);
+// let pass: Buffer;
+// let iv: Buffer;
 // Must be gen when the application starts.
 
 export default class Hash {
+    private pass: Buffer;
+    private iv: Buffer;
+    constructor(start_pass: Buffer, start_iv: Buffer) {
+        this.pass = start_pass;
+        this.iv = start_iv;
+    }
+    //Maybe need a refactor...
     encrypt(message: string): string {
-        const encrypt = createCipheriv("aes-256-gcm", pass, iv);
+        const encrypt = createCipheriv("aes-256-gcm", this.pass, this.iv);
         return encrypt.update(message, "utf-8", "base64");
     }
     desencrypt(message: string): string {
-        const decipher = createDecipheriv("aes-256-gcm", pass, iv);
+        const decipher = createDecipheriv("aes-256-gcm", this.pass, this.iv);
         return decipher.update(message, "utf-8", "base64");
     }
     compare(message: string, messageToCompare: string): boolean {
