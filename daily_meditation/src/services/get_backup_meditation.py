61a1e0fea7707c4ed84462e6d5644572e8ab5849
luishbarros <luishrbr@outlook.com>
2024-03-22 16:40:26 -0300

    backup_documentation

---
+++ daily_meditation/src/services/get_backup_meditation.py
@@ -0,0 +1,10 @@
+"""
+The backup should be a json file that contains the following structure:
+{
+    "weekday: "Monday",
+    "title": "The title of the meditation",
+    "content": "The content of the meditation",
+    image: "The image of the meditation",
+    "meditation": "Mulher"
+}
+"""
