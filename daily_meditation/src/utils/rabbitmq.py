d5a70e2613548b3549a282cf6ebf80ce2aee11b2
luishbarros <luishrbr@outlook.com>
2024-03-25 16:29:32 -0300

    message_broker

---
+++ daily_meditation/src/utils/rabbitmq.py
@@ -0,0 +1,46 @@
+import pika
+
+
+class Rabbitmq():
+    def __init__(self, callback):
+        self.__host = "localhost"
+        self.__port = 5672
+        self.__username = "guest"
+        self.__password = "guest"
+        self.__queue = "data"
+        self.__callback = callback
+        self.__channel = self.create_channel()
+
+    def create_channel(self):
+        connection_parameters = pika.ConnectionParameters(
+            host=self.__host,
+            port=self.__port,
+            credentials=pika.PlainCredentials(self.__username, self.__password)
+        )
+        channel = pika.BlockingConnection(connection_parameters).channel()
+        return channel
+
+    def consuming(self):
+        self.__channel.queue_declare(
+            queue=self.__queue,
+            durable=True
+        )
+        self.__channel.basic_consume(
+            queue=self.__queue,
+            auto_ack=True,
+            on_message_callback=self.__callback
+        )
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        self.__channel.start_consuming()
+
+    def publish(self, message):
+        self.__channel.basic_publish(
+            exchange="",
+            routing_key=self.__queue,
+            body=message,
+            properties=pika.BasicProperties(delivery_mode=2)
+        )
+
+    def minha_callback(self, ch, method, properties, body):
+        print(f" [x] Received {body}")
+
